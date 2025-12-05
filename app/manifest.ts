85d0163648d29c3771e3dda357f683d31256d010
twfeeqg-thub <ebnsalemalawal@gmail.com>
2025-12-06 02:16:22 +0300

    Refactor manifest to use local config file

--- app/manifest.ts
+++ app/manifest.ts
@@ -1,33 +1,37 @@
+// manifest.ts
 import { MetadataRoute } from 'next'
-import config from '../config.json'
+import config from './config.json'
 
 export default function manifest(): MetadataRoute.Manifest {
-  const { pwa } = config;
-
   return {
-    name: pwa.appName,
-    short_name: pwa.appShortName,
-    description: pwa.appDescription,
-    start_url: '/',
+    name: config.pwa.appName,
+    short_name: config.pwa.appShortName,
+    description: config.pwa.appDescription,
+    start_url: '/home', // <-- النقطة السحرية: يبدأ التطبيق من هنا بعد التثبيت
     display: 'standalone',
-    background_color: pwa.backgroundColor,
-    theme_color: pwa.themeColor,
+    background_color: config.pwa.backgroundColor,
+    theme_color: config.pwa.themeColor,
     icons: [
       {
-        src: '/favicon.ico', // يمكنك تغييره لاحقاً
+        src: '/favicon.ico',
         sizes: 'any',
         type: 'image/x-icon',
       },
       {
-        src: '/icon-192x192.png', // سنضيف هذه الأيقونات لاحقاً
-        sizes: '192x192',
-        type: 'image/png'
+        src: '/apple-icon.png',
+        sizes: '180x180',
+        type: 'image/png',
       },
       {
-        src: '/icon-512x512.png', // سنضيف هذه الأيقونات لاحقاً
+        src: '/icon-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/icon-512x512.png',
         sizes: '512x512',
-        type: 'image/png'
-      }
+        type: 'image/png',
+      },
     ],
   }
 }
